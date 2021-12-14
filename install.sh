982d43c25b57dc4d44a7f3dad8a2a34d8b5714a8
n00xt <60063989+n00xt@users.noreply.github.com>
2021-12-14 09:33:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install -y yarn sed
+sudo apt install -y yarn sed snmp snmpd snmp-mibs-downloader
 cd /home/pi/cardservice/ && yarn install
 
 sudo mv /home/pi/cardservice/kiosk.service /lib/systemd/system/kiosk.service
@@ -16,6 +16,10 @@ chmod +x /home/pi/cardservice/kiosk.sh
 #sudo daemon-reload
 sudo systemctl start kiosk.service && sudo systemctl enable kiosk.service
 
+sudo sed -i 's/mibs :/#mibs :/' /etc/snmp/snmp.conf
+sudo sed -i 's/agentAddress  udp:127.0.0.1:161/agentAddress udp:161/' /etc/snmp/snmpd.conf
+sudo sed -i 's/#rocommunity public  localhost/rocommunity public/' /etc/snmp/snmpd.conf
+
 
 sed -i 's/"orphan_rows_removed":true/"credit_card_enabled":false,"orphan_rows_removed":true,"profile_enabled":false/' /home/pi/.config/chromium/Default/Preferences
 
@@ -23,8 +27,8 @@ sed -i 's/"browser":{/"browser":{"clear_data":{"form_data":true,"hosted_apps_dat
 
 sed -i 's/"data_reduction"/"credentials_enable_autosignin":false,"credentials_enable_service":false,"data_reduction"/' /home/pi/.config/chromium/Default/Preferences
 
-mkdir logs
-touch ./logs/cronlog
+mkdir /home/pi/cardservice/logs
+touch /home/pi/cardservice/logs/cronlog
 chmod +x /home/pi/cardservice/update.sh
 
 sudo /bin/bash -c 'echo "0 0 * * 0 /sbin/shutdown -r now" >> /etc/crontab'
